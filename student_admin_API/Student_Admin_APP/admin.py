15b6f2f176a74ef0c1889cea85dd78c56fe88cbb
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-24 17:45:33 +0200

    student admin model created

--- student_admin_API/Student_Admin_APP/admin.py
+++ student_admin_API/Student_Admin_APP/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student_admin
+from .models import Student_administration
 
 # Register your models here.
-admin.site.register(Student_admin)
+admin.site.register(Student_administration)
