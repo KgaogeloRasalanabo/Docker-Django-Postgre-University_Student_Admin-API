3028dc969d51d9c9efb5f29bfa7125969b6d9439
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-27 11:44:39 +0200

    to convert data

---
+++ student_admin_API/Student_Admin_APP/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from Student_Admin_APP.models import Student_administration
+
+
+class Student_administration_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student_administration
+        field = ('name', 'surname', 'id_no', 'course_id', 'course_name', 'Course_code')
