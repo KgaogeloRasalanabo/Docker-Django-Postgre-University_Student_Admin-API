779f798d9bed7c04c64e23be671b6af38ce06609
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-28 18:44:57 +0200

    added all fields

--- student_admin_API/Student_Admin_APP/urls.py
+++ student_admin_API/Student_Admin_APP/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 from Student_Admin_APP import views
 
 
 urlpatterns = [
-    path(r'^student_administration$', views.student_administration_Api),
-    path(r'^student_administration/([0-9]+)$', views.student_administration_Api)
+    re_path(r'^student_administration$', views.student_administration_Api),
+    re_path(r'^student_administration/([0-9]+)$', views.student_administration_Api),
 ]
