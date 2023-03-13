0521b24b7292058b412b3074a9fa8be23f10576f
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-03-13 13:10:34 +0200

    added Apps & PostgreSQL

--- student_admin_API/student_admin_API/settings.py
+++ student_admin_API/student_admin_API/settings.py
@@ -84,13 +84,14 @@ WSGI_APPLICATION = 'student_admin_API.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'uni_std_admin',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'PORT': '5433',
-    }
+    'default': {	
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "uni_std_admin",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5433,  # default postgres port	
+  }
 
 }
 
